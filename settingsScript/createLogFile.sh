3d74915297701a4b251d696bb5f138fe44d20363
la-strole <sovaleto48@gmail.com>
2023-08-08 20:26:07 +0400

    Sanity

--- settingsScript/createLogFile.sh
+++ settingsScript/createLogFile.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-touch /home/zhenya/webhook/logfile.txt
+touch /home/zhenya/webhook/webhook.log
 # MAKE SHURE TO DO IT. 
 # Because Flask app run by gunicorn.service from webhook user, 
 #it can not write in your direcory. And It raises Error.
-chmod a=rw /home/zhenya/webhook/logfile.txt
-
-touch /home/zhenya/webhook/logfile_Flask.txt
-chmod a=rw /home/zhenya/webhook/logfile_Flask.txt
+chmod a=rw /home/zhenya/webhook/webhook.log
