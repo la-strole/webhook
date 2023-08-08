328ab8829c832536df185be71ecc957cc7d170cd
la-strole <sovaleto48@gmail.com>
2023-08-08 21:31:31 +0400

    Lint python files

--- webhookHandler.py
+++ webhookHandler.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import requests
-from dockerHookHandler import image_manipulation 
+from dockerHookHandler import image_manipulation
 import datetime
 import pytz
 from logging_config import logger_module1 as logger
@@ -11,25 +11,26 @@ uuid_dokcerhub = 'c5c6229f-c846-48a0-b941-501e7c8d77ea'
 
 app = Flask(__name__)
 
-@app.route(f"/{uuid_dokcerhub}", methods=["POST"])
+
+@app.route(f"/{uuid_dokcerhub}", methods=["POST"])  # type: ignore
 def webhook_handler():
     logger.info('Get webhook request from docker')
     json_data = request.json
 
     if json_data is None:
-         logger.error('Can not parse json data')
-         return jsonify({'error': 'Invalid JSON data'}), 400
-    
+        logger.error('Can not parse json data')
+        return jsonify({'error': 'Invalid JSON data'}), 400
+
     # Access specific values from the JSON data
     # https://docs.docker.com/docker-hub/webhooks/
     callback_url = json_data.get('callback_url')
-    repo_name =  json_data.get('repository').get('repo_name')
+    repo_name = json_data.get('repository').get('repo_name')
     logger.debug(f"callback_url:{callback_url}, repo_name:{repo_name}")
     if repo_name == 'eugeneparkhom/pomodoro':
         options = {'-p': '8123:80'}
         commands = []
         result = image_manipulation(repo_name, options, commands)
-    elif repo_name == 'eugeneparkhom/weatherbot':
+    else:  # repo_name == 'eugeneparkhom/weatherbot':
         options = {'--env-file': 'path_to_env'}
         commands = ['make', '--file', '/home/Makefile', 'run']
         result = image_manipulation(repo_name, options, commands)
@@ -41,22 +42,25 @@ def webhook_handler():
     if result == 0:
         # response body https://docs.docker.com/docker-hub/webhooks/
         body = {
-        "state": "success",
-        "description": f"{repo_name} successfully installed at {timestamp.strftime('%H:%M %Z %d.%m.%Y')}",
-        "context": "Continuous delivery of Docker",
-        "target_url": f"https://webhook.dobryzhuk.com/{uuid_dokcerhub}"
-        }
+            "state": "success",
+            "description": f"{repo_name} successfully installed "
+                           f"at {timestamp.strftime('%H:%M %Z %d.%m.%Y')}",
+            "context": "Continuous delivery of Docker",
+            "target_url": f"https://webhook.dobryzhuk.com/{uuid_dokcerhub}"
+            }
         requests.post(callback_url, json=body, headers=response_headers)
         logger.debug("Sent successful webhook POST request to dockerhub")
         return None
     else:
         # response body https://docs.docker.com/docker-hub/webhooks/
         body = {
-        "state": "failure",
-        "description": f"{repo_name} failed installed at {timestamp.strftime('%H:%M %Z %d.%m.%Y')}. {result}",
-        "context": "Continuous delivery of Docker",
-        "target_url": f"https://webhook.dobryzhuk.com/{uuid_dokcerhub}"
-        }
+            "state": "failure",
+            "description": f"{repo_name} failed installed at"
+                           f"{timestamp.strftime('%H:%M %Z %d.%m.%Y')}."
+                           f"{result}",
+            "context": "Continuous delivery of Docker",
+            "target_url": f"https://webhook.dobryzhuk.com/{uuid_dokcerhub}"
+            }
         requests.post(callback_url, json=body, headers=response_headers)
         logger.debug("Sent failure webhook POST request to dockerhub")
         return None
@@ -66,5 +70,5 @@ def webhook_handler():
 def webhook_handler_response():
     with open('webhook.log', 'r') as file:
         content = [line.strip() for line in file]
-        
+
     return render_template('log_template.html', log_content=content)
