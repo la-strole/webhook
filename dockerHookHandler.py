7445018891909680a5073472d77d404ecbaac4e1
la-strole <sovaleto48@gmail.com>
2023-08-08 22:42:42 +0400

    Tested

--- dockerHookHandler.py
+++ dockerHookHandler.py
@@ -3,26 +3,36 @@ import time
 
 from logging_config import logger_module2 as logger
 
-
+# Emulator for test
+"""
+class SubprocessEmulator():
+    def __init__(self) -> None:
+        self.stderr = ''
+"""
 def image_manipulation(imagename, options, commands):
 
     # Stop container
     stop_command = ['sudo', 'docker', 'stop', f'{imagename.split("/")[1]}']
+    
     stop_result = subprocess.run(stop_command,
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE,
                                  text=True)
+    
+    #  stop_result = SubprocessEmulator()
     if not stop_result.stderr:
         logger.info(f"Container {imagename} stopped.")
         # Remove image
         # wait for 5 seconds
-        time.sleep(5)
+        time.sleep(7)
         remove_image_command = ['sudo',
                                 'docker', 'image', 'rm', f'{imagename}']
+        #  remove_image_result = SubprocessEmulator()
         remove_image_result = subprocess.run(remove_image_command,
                                              stdout=subprocess.PIPE,
                                              stderr=subprocess.PIPE,
-                                             text=True)
+                                             text=True
+                                            )
         if not remove_image_result.stderr:
             logger.info(f"Image {imagename} removed.")
         else:
@@ -33,10 +43,12 @@ def image_manipulation(imagename, options, commands):
 
     # Pull new image from dockerhub and run container
     pull_image_command = ['sudo', 'docker', 'image', 'pull', imagename]
+    #  pull_image_result = SubprocessEmulator()
     pull_image_result = subprocess.run(pull_image_command,
                                        stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE, text=True
-                                       )
+                                       stderr=subprocess.PIPE,
+                                       text=True
+                                      )
     if not pull_image_result.stderr:
         logger.info(f'Successfully pull image {imagename} from dockerhub')
 
@@ -57,19 +69,20 @@ def image_manipulation(imagename, options, commands):
         run_contaiter_command.extend(option_list)
         run_contaiter_command.append(imagename)
         run_contaiter_command.extend(commands)
+        #  run_container_result = SubprocessEmulator()
         run_container_result = subprocess.run(run_contaiter_command,
                                               stdout=subprocess.PIPE,
-                                              stderr=subprocess.PIPE, text=True
-                                              )
-        if not run_container_result:
+                                              stderr=subprocess.PIPE,
+                                              text=True)
+        if not run_container_result.stderr:
             logger.info(f'Successfully run conatiner from image {imagename}')
         else:
             logger.error(f"Failed to run container from image {imagename}: "
-                         "{run_container_result.stderr}")
+                         f"{run_container_result.stderr}")
 
         return 0
 
     else:
         logger.error(f'Failed pull image {imagename} from dockerhub:'
-                     '{pull_image_result.stderr}')
+                     f'{pull_image_result.stderr}')
         return -1
---
+++ test/test.py
@@ -0,0 +1,33 @@
+import subprocess
+import json
+
+# Create fake POST request from dockerhub
+body = {"callback_url": "https://registry.hub.docker.com/u/svendowideit/testhook/hook/2141b5bi5i5b02bec211i4eeih0242eg11000a/",
+        "push_data": {
+            "pushed_at": 1417566161,
+            "pusher": "trustedbuilder",
+            "tag": "latest"
+        },
+        "repository": {
+            "comment_count": 0,
+            "date_created": 1417494799,
+            "description": "",
+            "dockerfile": "#\n# BUILD\u0009\u0009docker build -t svendowideit/apt-cacher .\n# RUN\u0009\u0009docker run -d -p 3142:3142 -name apt-cacher-run apt-cacher\n#\n# and then you can run containers with:\n# \u0009\u0009docker run -t -i -rm -e http_proxy http://192.168.1.2:3142/ debian bash\n#\nFROM\u0009\u0009ubuntu\n\n\nVOLUME\u0009\u0009[/var/cache/apt-cacher-ng]\nRUN\u0009\u0009apt-get update ; apt-get install -yq apt-cacher-ng\n\nEXPOSE \u0009\u00093142\nCMD\u0009\u0009chmod 777 /var/cache/apt-cacher-ng ; /etc/init.d/apt-cacher-ng start ; tail -f /var/log/apt-cacher-ng/*\n",
+            "full_description": "Docker Hub based automated build from a GitHub repo",
+            "is_official": False,
+            "is_private": True,
+            "is_trusted": True,
+            "name": "testhook",
+            "namespace": "svendowideit",
+            "owner": "svendowideit",
+            "repo_name": "eugeneparkhom/pomodoro",
+            "repo_url": "https://registry.hub.docker.com/u/svendowideit/testhook/",
+            "star_count": 0,
+            "status": "Active"
+        }
+        }
+
+body_json_string = json.dumps(body)
+url = "http://127.0.0.1:5000/c5c6229f-c846-48a0-b941-501e7c8d77ea"
+command = ['curl', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', body_json_string, url]
+subprocess.run(command)
