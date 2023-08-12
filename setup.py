3ac22cbd1af39360a7b570f49186997286eeff6d
la-strole <sovaleto48@gmail.com>
2023-08-13 01:26:37 +0400

    the ability to change url_token, callback_url

--- setup.py
+++ setup.py
@@ -15,29 +15,50 @@ def rewrite_configuration(configuration):
 token = uuid.uuid4()
 
 # Display URL for setting up DockerHub webhook
-print(f"Your URL to configure on DockerHub as a webhook:\n"
-      f"<domain_name_to_handle_webhook>/token\ngenerated token:\n{token}")
+print(f"The URL you need to set up on DockerHub as a webhook:\n"
+      f"\n<domain_name_to_handle_webhook>/token\n")
 
 # Configure ./application_conf.json
 with open('application_conf.json', 'r') as file:
     # If configuration already exists
     try:
         configuration = json.load(file)
-        url_token = configuration.get('url_token')
-        if url_token:
-            rewrite_solution = input(
-                f"Token configuration already exists:\n"
-                f"url token = {url_token}\n"
-                f"Do you want to update it? [y/n]?"
-            )
-            if rewrite_solution in ['y', 'Y']:
+        old_url_token = configuration.get('url_token')
+        old_callback_url = configuration.get('callback_url')
+        print(f'Your current configuration:\n'
+              f'url_token: {old_url_token}\n'
+              f'callback_url: {old_callback_url}\n')
+        if old_url_token:
+            solution = input('Do you want to change url_token? y/n: ')
+            if solution in ['y', 'Y']:
                 configuration['url_token'] = str(token)
-                rewrite_configuration(configuration)
+                print("The URL token was successfully updated.")
             else:
-                print("Alright, configuration remains unchanged")
+                print("Alright, url_token remains unchanged")
         else:
             configuration['url_token'] = str(token)
+            print(f"Because your URL token didn't exist, "
+                  f"a new token was created with the value:\n{token}")
+        if old_callback_url:
+            solution = input('Do you want to change callback_url? y/n: ')
+            if solution in ['y', 'Y']:
+                configuration['callback_url'] = input(
+                    'Specify a new value for the callback_url: ')
+                print("The callback_url was successfully updated.")
+            else:
+                print("Alright, callback_url remains unchanged")
+        else:
+            solution = input('Do you want to set callback_url? y/n: ')
+            if solution in ['y', 'Y']:
+                configuration['callback_url'] = input(
+                    'Specify a new value for the callback_url: ')
+                print("The callback_url was successfully updated.")
+            else:
+                print("Alright, callback_url remains unchanged")
+        if (old_url_token != configuration.get('url_token') or 
+            old_callback_url != configuration.get('callback_url')):
             rewrite_configuration(configuration)
+
 
     except json.JSONDecodeError as e:
         print(f"Error loading configuration from "
